d5159a5f82f833ba02110ccfe852b087d9593d51
ArseneBukasa <50106994+ArseneBukasa@users.noreply.github.com>
2019-04-30 05:42:55 -0700

    Update liste.php

--- liste.php
+++ liste.php
@@ -21,18 +21,7 @@ $result=mysqli_query($mysqli,"select * from utilisateur order by Num desc");
 	<td>Votre Age</td>
 	<td>Mise à jour</td>
 </tr>
-	<?php
-	while ($res=mysqli_fetch_array($result)) {
-	# code...
-	echo "<tr>";
-	echo "<td>".$res['Num']."</td>";
-	echo "<td>".$res['nom']."</td>";
-	echo "<td>".$res['Prenom']."</td>";
-	echo "<td>".$res['Sexe']."</td>";
-	echo "<td>".$res['Age']."</td>";
-	echo "<td><a href=\"modifier.php?id=$res[Num]\">Modifier</a>│<a href=\"supprimer.php?id=$res[Num]\" onClick=\"return confirm('voulez-vous vraiment supprimé cet utilisateur?')\">Supprimer</a></td>";
-}
-
+	
 ?>
 </table>
 </body>
